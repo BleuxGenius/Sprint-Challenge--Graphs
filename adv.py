5d3fd3e1cede20bbaf927ac643806b3479d728bf
Dani Blackwell <bleuxgenius@gmail.com>
2020-06-12 12:41:44 -0500

    still working

--- adv.py
+++ adv.py
@@ -2,11 +2,40 @@ from room import Room
 from player import Player
 from world import World
 
+
 import random
 from ast import literal_eval
 
 # Load world
 world = World()
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
@@ -28,6 +57,42 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+# when the end of a path is reached, player needs to backtrack in the opposite direction 
+reversed_direction = {
+    'n':'s',
+    's':'n',
+    'w':'e',
+    'e':'w'
+}
+
+def traversal_map(starting_room, visited_rooms = set()):
+
+    starting_path = []
+        # get all possible exists in the current room 
+    for direction in player.get_exits():
+        # when player travels to room in direction of the exit
+        player.travel(direction)
+        # check if new room has been visitied 
+        if player.current_room.id not in visited_rooms:
+        # room has not been visited 
+        #  mark visited 
+            visited_rooms.add(player.current_room.id)
+        # add new direction to the path 
+            starting_path.append(direction)
+        # use recursion with new current_room and add to the path 
+            starting_path = starting_path + traversal_map(player.current_room.id, visited_rooms)
+        # retrace steps and go to a diffrent room 
+            player.travel(reversed_direction[direction])
+        # add retraced steps to he path to keep the track of steps 
+            starting_path.append(reversed_direction[direction])
+        else:
+            # Room already visited so retrace steps and go to a diffrent room 
+            player.travel(reversed_direction[direction])
+    return starting_path
+    
+
+
+
 
 
 
--- player.py
+++ player.py
@@ -9,3 +9,32 @@ class Player:
                 next_room.print_room_description(self)
         else:
             print("You cannot move in that direction.")
+    def get_exits(self):
+        exits = []
+        if self.n_to is not None:
+            exits.append("n")
+        if self.s_to is not None:
+            exits.append("s")
+        if self.w_to is not None:
+            exits.append("w")
+        if self.e_to is not None:
+            exits.append("e")
+        return exits
+    def get_exits_string(self):
+        return f"Exits: [{', '.join(self.get_exits())}]"
+    def connect_rooms(self, direction, connecting_room):
+        if direction == "n":
+            self.n_to = connecting_room
+            connecting_room.s_to = self
+        elif direction == "s":
+            self.s_to = connecting_room
+            connecting_room.n_to = self
+        elif direction == "e":
+            self.e_to = connecting_room
+            connecting_room.w_to = self
+        elif direction == "w":
+            self.w_to = connecting_room
+            connecting_room.e_to = self
+        else:
+            print("INVALID ROOM CONNECTION")
+            return None
